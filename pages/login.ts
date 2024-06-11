a87949780f56ab63c09ce9e87af343d941685e10
Kanimozhi3224 <Kanimozhi3224@gmail.com>
2024-06-11 14:28:56 +0530

    loginpageCredintials

---
+++ pages/login.ts
@@ -0,0 +1,26 @@
+import {Page} from "@playwright/test"
+
+export default class loginPage{
+
+    constructor(public page: Page) {
+
+    }
+
+    async enterEmail(email:string){
+
+        this.page.locator("#input-email").type(email);
+
+    }
+    async enterPassword(password:string){
+
+        this.page.locator("#input-password").type(password);
+
+    }
+    async enterEmail(email:string){
+
+        this.page.click("input[value='Login']");
+
+    }
+
+
+}
