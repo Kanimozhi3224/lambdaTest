19e676629886387c64c681906cff701f1bce2b6c
Kanimozhi3224 <Kanimozhi3224@gmail.com>
2024-06-11 14:20:33 +0530

    login page

---
+++ pages/register.ts
@@ -0,0 +1,56 @@
+import { Page } from "@playwright/test";
+
+export default class registerPage{
+
+    constructor(public page: Page) {
+        
+    }
+    async enterFirstName(firstname: string) {
+
+        await this.page.locator("#input-firstname").type(firstname)
+    
+    }
+
+    async enterLastName(lastname: string) {
+
+        await this.page.locator("#input-lastname").type(lastname);
+    
+    }
+
+
+    async enterEmail(email: string) {
+
+        await this.page.locator("#input-email").type(email);
+    
+    }
+
+    async enterTelephone(telephone: string) {
+
+        await this.page.locator('input[name="telephone"]').type(telephone); 
+        
+             }
+    async enterPassword(password: string) {
+
+        await this.page.locator("#input-password").type(password);
+    
+    }
+    async enterConfirmPassword(password: string) {
+
+        await this.page.locator("#input-confirm").type(password);
+    
+    }
+
+    async issubscribedChecked(){
+
+         return await this.page.locator("#input-newsletter-no").isChecked();
+    }
+    async clickContinueBtn() {
+
+        await this.page.click("input[value='Continue']");
+    
+    }
+
+ 
+
+
+}
